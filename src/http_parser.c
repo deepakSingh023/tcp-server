a53544ecbeba7186b75a2252e3bf807a45a89955
deepakSingh023 <dsinghrana71@gmail.com>
2026-09-21 19:38:32 +0530

    the main is complete with the req check so that we can know if its complete also i have a parser to check if the req is complete

--- src/http_parser.c
+++ src/http_parser.c
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <ctype.h>
 
+
 void http_parser_init(HttpParser *parser)
 {
     parser->state = HTTP_PARSING_HEADERS;
@@ -15,24 +16,138 @@ void http_parser_init(HttpParser *parser)
     parser->content_length = 0;
 }
 
+
 int http_parser_execute(
     HttpParser *parser,
     Buffer *buffer
-){
-    if(parser->state = HTTP_COMPLETE){
+)
+{
+
+    if (parser->state == HTTP_COMPLETE) {
         return 1;
     }
+
 
     if (parser->state == HTTP_ERROR) {
         return -1;
     }
 
-    if(parser->state = HTTP_PARSING_HEADERS){
+    if (parser->state == HTTP_PARSING_HEADERS) {
 
-        char *found = memme(buffer->data+parser->scan_position,buffer->length - parser->content_length, "\r\n\r\n",4);
-        if(found == NULL){
-            parser->scan_position= buffer->length;
+
+        size_t search_start = parser->scan_position;
+
+        if (search_start >= 3) {
+            search_start -= 3;
+        } else {
+            search_start = 0;
         }
-    }
-}
 
+        if (search_start > buffer->length) {
+            search_start = buffer->length;
+        }
+
+
+        char *found = memmem(
+            buffer->data + search_start,
+            buffer->length - search_start,
+            "\r\n\r\n",
+            4
+        );
+
+        if (found == NULL) {
+
+            parser->scan_position = buffer->length;
+
+            return 0;
+        }
+
+        parser->header_end =
+            (size_t)(found - buffer->data);
+
+        parser->body_start =
+            parser->header_end + 4;
+
+        char *content_length_header = memmem(
+            buffer->data,
+            parser->header_end,
+            "Content-Length:",
+            15
+        );
+
+
+        if (content_length_header == NULL) {
+
+            parser->content_length = 0;
+
+            parser->state = HTTP_COMPLETE;
+
+            return 1;
+        }
+
+
+        size_t length_start =
+            (size_t)(content_length_header - buffer->data) + 15;
+
+
+        // Skip spaces
+
+        while (
+            length_start < parser->header_end &&
+            buffer->data[length_start] == ' '
+        ) {
+            length_start++;
+        }
+
+
+        size_t content_length = 0;
+
+        size_t i = length_start;
+
+
+        // Read digits
+
+        while (
+            i < parser->header_end &&
+            buffer->data[i] >= '0' &&
+            buffer->data[i] <= '9'
+        ) {
+
+            content_length =
+                content_length * 10 +
+                (buffer->data[i] - '0');
+
+            i++;
+        }
+
+
+        parser->content_length = content_length;
+
+        if (parser->content_length == 0) {
+
+            parser->state = HTTP_COMPLETE;
+
+            return 1;
+        }
+
+
+        parser->state = HTTP_PARSING_BODY;
+    }
+
+    if (parser->state == HTTP_PARSING_BODY) {
+
+        size_t body_received =
+            buffer->length - parser->body_start;
+
+
+        if (body_received < parser->content_length) {
+            return 0;
+        }
+        parser->state = HTTP_COMPLETE;
+
+        return 1;
+    }
+
+
+    return 0;
+}
--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#define _GNU_SOURCE
 
 #include <sys/epoll.h>
 #include <stdio.h>
@@ -7,133 +8,102 @@
 #include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include "connection.h"
-#include <arpa/inet.h>// inet_addr(), inet_pton()
-#include <stddef.h> 
-#include "thread_pool.h"
+#include <arpa/inet.h>
+#include <stddef.h>
 #include <fcntl.h>
+
+#include "connection.h"
+#include "thread_pool.h"
+
 #define MAX_EVENTS 64
-#define _GNU_SOURCE
-#include <string.h>
-int make_nonBlocking(int fd){
 
-    int flag = fcntl(fd , F_GETFL , 0);
 
-    if(flag == -1){
+int make_nonBlocking(int fd)
+{
+    int flag = fcntl(fd, F_GETFL, 0);
+
+    if (flag == -1) {
         return -1;
     }
 
-    if(fcntl(fd, F_SETFL, flag | O_NONBLOCK) == -1){
+    if (fcntl(fd, F_SETFL, flag | O_NONBLOCK) == -1) {
         return -1;
     }
 
     return 0;
-
-}
-
-int helper_funtion(Buffer *buffer){
-    char *found = memmem(
-        buffer->data,
-        buffer->length,
-        "\r\n\r\n",
-        4
-    ); 
-
-    if(found == NULL){
-        return -1;
-    }
-
-    size_t header_end = (char *)found - buffer->data;
-    size_t body_start = header_end + 4;
-
-    char *content_length = memmem(
-        buffer->data,
-        header_end,
-        "Content-Length:",
-        15
-    );
-    if (content_length == NULL) {
-        return -1;
-    }
-
-    size_t cont = (char *)content_length - buffer->data;
-
-    size_t length_start = cont + 15;
-
-    size_t content_len = 0;
-
-    for (size_t i = length_start; i < header_end; i++) {
-    
-        if (buffer->data[i] < '0' ||
-            buffer->data[i] > '9') {
-            break;
-        }
-    
-        content_len =
-            content_len * 10 +
-            (buffer->data[i] - '0');
-    }
-
-    return 0;
-
 }
 
 
-int main(){
-
+int main()
+{
     ThreadPool pool;
 
     thread_pool_init(&pool);
 
-    //create the tcp listenign socket
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
 
-    if(server_fd == -1){
+    if (server_fd == -1) {
         perror("socket failed");
         return 1;
     }
-    
-    //create an adress 
+
+
     struct sockaddr_in address;
+
+    memset(&address, 0, sizeof(address));
 
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(8080);
 
 
-    //bind the socket
-    if(bind(server_fd,(struct sockaddr* )&address, sizeof(address)) == -1){
+    if (bind(
+        server_fd,
+        (struct sockaddr *)&address,
+        sizeof(address)
+    ) == -1) {
+
         perror("bind failed");
+        close(server_fd);
         return 1;
-    };
+    }
 
 
-    //listen
-    if(listen(server_fd , 10 ) == -1){
+    if (listen(server_fd, 10) == -1) {
+
         perror("listen failed");
         close(server_fd);
         return 1;
     }
 
-    if(make_nonBlocking(server_fd) == -1){
+
+    if (make_nonBlocking(server_fd) == -1) {
+
         perror("make_nonBlocking failed");
         close(server_fd);
         return 1;
     }
 
+
+
     int epoll_fd = epoll_create1(0);
 
-    if(epoll_fd == -1){
+    if (epoll_fd == -1) {
+
         perror("epoll_create1 failed");
         close(server_fd);
         return 1;
     }
 
-
-    //epoll setup listening on the fd 3 which is the socket
     struct epoll_event event;
-    event.events = EPOLLIN ;
+
+    memset(&event, 0, sizeof(event));
+
+    event.events = EPOLLIN;
+
+    // NULL means this is the listening socket.
     event.data.ptr = NULL;
+
 
     if (epoll_ctl(
         epoll_fd,
@@ -141,144 +111,272 @@ int main(){
         server_fd,
         &event
     ) == -1) {
+
         perror("epoll_ctl listener");
+
         close(epoll_fd);
         close(server_fd);
+
         return 1;
     }
-        
+
+
     struct epoll_event events[MAX_EVENTS];
 
-    while(1){
+    while (1) {
 
-        //the event count never becomes  because we havent given it a timeout it will sleep while there is nothing also it give -1 when there is an error 
-        int event_count = epoll_wait(epoll_fd, events, MAX_EVENTS, -1);
-        if(event_count == -1){
-            //the EINTR error is the signal that is sent when it is interuppted by a signal and we continue because we want to continue
+        int event_count = epoll_wait(
+            epoll_fd,
+            events,
+            MAX_EVENTS,
+            -1
+        );
+
+
+        if (event_count == -1) {
+
             if (errno == EINTR) {
                 continue;
             }
+
             perror("epoll_wait");
             break;
         }
 
-        for(int i = 0 ; i < event_count; i++){
+
+        for (int i = 0; i < event_count; i++) {
 
             Connection *conn = events[i].data.ptr;
 
-            if(conn == NULL){
-                int client_fd = accept(server_fd, NULL, NULL);
+            if (conn == NULL) {
 
-                if (client_fd == -1) {
+                int client_fd;
 
-                    if (errno == EAGAIN ||
-                        errno == EWOULDBLOCK) {
-                        continue;
-                    }
+                while (1) {
 
-                    perror("accept");
-                    continue;
-                }
-
-                if(make_nonBlocking(client_fd) == -1){
-                    perror("make_nonBlocking failed");
-                    close(client_fd);
-                    continue;
-                }
-
-                conn = malloc(sizeof(Connection));
-
-                if( conn == NULL){
-                    perror("malloc failed in main");
-                    return -1;
-                }
-
-                if(connection_init(conn,client_fd) == -1){
-                    free(conn);
-                    close(client_fd);
-                    perror("failed to initiate");
-                    return -1;
-
-                }
-                
-
-                struct epoll_event client_event;
-
-                client_event.events = EPOLLIN| EPOLLET;
-                client_event.data.ptr = conn;
-
-                if (epoll_ctl(
-                    epoll_fd,
-                    EPOLL_CTL_ADD,
-                    conn->fd,
-                    &client_event
-                ) == -1) {
-                    connection_free(conn);
-                    free(conn);
-                    close(client_fd);
-                    perror("epoll_ctl client");
-                    continue;
-                }
-
-                printf("Client connected: fd=%d\n", client_fd);
-
-            
-            }else{
+                    client_fd = accept(
+                        server_fd,
+                        NULL,
+                        NULL
+                    );
 
 
-                char Buffer[4096];
+                    if (client_fd == -1) {
 
-                while(1){
+                        if (errno == EAGAIN ||
+                            errno == EWOULDBLOCK) {
 
-                    ssize_t receiver_data = recv(conn->fd, Buffer, sizeof(Buffer) - 1, 0);
-
-
-                    if(receiver_data > 0){
-
-                        if(buffer_append(&conn->input,Buffer,receiver_data) == -1){
-                            perror("not appended");
-                            return -1;
+                            break;
                         }
+
+                        if (errno == EINTR) {
+                            continue;
+                        }
+
+                        perror("accept");
+                        break;
+                    }
+
+                    if (make_nonBlocking(client_fd) == -1) {
+
+                        perror("make_nonBlocking failed");
+
+                        close(client_fd);
                         continue;
                     }
 
-                    if (receiver_data == 0) {
-                        // client closed connection
-                    
-                        epoll_ctl(epoll_fd, EPOLL_CTL_DEL, conn->fd, NULL);
-                    
+                    conn = malloc(sizeof(Connection));
+
+                    if (conn == NULL) {
+
+                        perror("malloc failed");
+
+                        close(client_fd);
+                        continue;
+                    }
+
+                    if (connection_init(conn, client_fd) == -1) {
+
+                        perror("connection_init failed");
+
+                        free(conn);
+                        close(client_fd);
+
+                        continue;
+                    }
+
+                    struct epoll_event client_event;
+
+                    memset(
+                        &client_event,
+                        0,
+                        sizeof(client_event)
+                    );
+
+
+                    client_event.events =
+                        EPOLLIN | EPOLLET;
+
+                    client_event.data.ptr = conn;
+
+
+                    if (epoll_ctl(
+                        epoll_fd,
+                        EPOLL_CTL_ADD,
+                        conn->fd,
+                        &client_event
+                    ) == -1) {
+
+                        perror("epoll_ctl client");
+
+                        connection_free(conn);
+                        free(conn);
+                        close(client_fd);
+
+                        continue;
+                    }
+                    printf(
+                        "Client connected: fd=%d\n",
+                        client_fd
+                    );
+                }
+
+
+                continue;
+            }
+
+            while (1) {
+
+                char buffer[4096];
+                ssize_t received = recv(
+                    conn->fd,
+                    buffer,
+                    sizeof(buffer),
+                    0
+                );
+
+                if (received > 0) {
+
+                    if (buffer_append(
+                        &conn->input,
+                        buffer,
+                        received
+                    ) == -1) {
+
+                        perror("buffer_append failed");
+
+                        epoll_ctl(
+                            epoll_fd,
+                            EPOLL_CTL_DEL,
+                            conn->fd,
+                            NULL
+                        );
+
+                        close(conn->fd);
+
+                        connection_free(conn);
+                        free(conn);
+
+                        break;
+                    }
+
+                    int result = http_parser_execute(
+                        &conn->parser,
+                        &conn->input
+                    );
+
+
+                    if (result == 1) {
+
+                        Task task;
+
+                        task.conn = conn;
+
+                        enqueue(
+                            &pool.queue,
+                            task
+                        );
+
+
+                        printf(
+                            "Request ready for worker: fd=%d\n",
+                            conn->fd
+                        );
+
+                    }
+
+                    if (result == -1) {
+
+                        fprintf(
+                            stderr,
+                            "HTTP parser error on fd=%d\n",
+                            conn->fd
+                        );
+                        epoll_ctl(
+                            epoll_fd,
+                            EPOLL_CTL_DEL,
+                            conn->fd,
+                            NULL
+                        );
                         close(conn->fd);
                         connection_free(conn);
                         free(conn);
-                    
+
                         break;
                     }
-
-                    if (errno == EAGAIN || errno == EWOULDBLOCK) {
-                        // nothing currently left
-                        break;
-                    }
-
-                    perror("recv");
-                    
-                    epoll_ctl(epoll_fd, EPOLL_CTL_DEL, conn->fd, NULL);
-                    
-                    close(conn->fd);
-                    connection_free(conn);
-                    free(conn);
-                    
-                    break;
-
-                    break;
-
-
-
+                    continue;
                 }
 
 
+                if (received == 0) {
+
+                    printf(
+                        "Client disconnected: fd=%d\n",
+                        conn->fd
+                    );
+
+
+                    epoll_ctl(
+                        epoll_fd,
+                        EPOLL_CTL_DEL,
+                        conn->fd,
+                        NULL
+                    );
+
+                    close(conn->fd);
+
+                    connection_free(conn);
+                    free(conn);
+
+                    break;
+                }
+
+                if (errno == EAGAIN ||
+                    errno == EWOULDBLOCK) {
+                    break;
+                }
+
+                perror("recv");
+
+
+                epoll_ctl(
+                    epoll_fd,
+                    EPOLL_CTL_DEL,
+                    conn->fd,
+                    NULL
+                );
+
+
+                close(conn->fd);
+
+                connection_free(conn);
+                free(conn);
+
+                break;
             }
         }
     }
+
 
     close(epoll_fd);
     close(server_fd);
